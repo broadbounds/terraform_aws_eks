1ce6618139b5c815e3c3ee9d921787eeb93f1869
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-24 12:27:45 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_vpc" "vpc" {
    }
    enable_dns_hostnames = true
 }
+
 # We create a public subnet in AZ 1
 # Instances will have a dynamic public IP and be accessible via the internet gateway
 resource "aws_subnet" "public_subnet_1" {
@@ -30,6 +31,7 @@ resource "aws_subnet" "public_subnet_1" {
    }
    map_public_ip_on_launch = true
 }
+
 # We create a public subnet in AZ 2
 # Instances will have a dynamic public IP and be accessible via the internet gateway
 resource "aws_subnet" "public_subnet_2" {
@@ -45,6 +47,7 @@ resource "aws_subnet" "public_subnet_2" {
    }
    map_public_ip_on_launch = true
 }
+
 # We create a private subnet in AZ 1
 # Instances will not be accessible via the internet gateway
 resource "aws_subnet" "private_subnet_1" {
@@ -59,6 +62,7 @@ resource "aws_subnet" "private_subnet_1" {
       "kubernetes.io/cluster/${var.cluster_name}" = "shared"
    }
 }
+
 # We create a private subnet in AZ 2
 # Instances will not be accessible via the internet gateway
 resource "aws_subnet" "private_subnet_2" {
@@ -73,6 +77,7 @@ resource "aws_subnet" "private_subnet_2" {
       "kubernetes.io/cluster/${var.cluster_name}" = "shared"
    }
 }
+
 # We create an internet gateway
 # Allows communication between our VPC and the internet
 resource "aws_internet_gateway" "internet_gateway" {
@@ -84,6 +89,7 @@ resource "aws_internet_gateway" "internet_gateway" {
       Name = "internet-gateway",
    }
 }
+
 # We need 1 public routetable because it is associated to the same intenet gateway id
 # We create a route table with target as our internet gateway and destination as "internet"
 # Set of rules used to determine where network traffic is directed
@@ -101,6 +107,7 @@ resource "aws_route_table" "IG_route_table" {
       Name = "IG-route-table"
    }
 }
+
 # We associate our route table to the public subnet in AZ 1
 # Makes the subnet public because it has a route to the internet via our internet gateway
 resource "aws_route_table_association" "associate_routetable_to_public_subnet_1" {
@@ -111,6 +118,7 @@ resource "aws_route_table_association" "associate_routetable_to_public_subnet_1"
    subnet_id = aws_subnet.public_subnet_1.id
    route_table_id = aws_route_table.IG_route_table.id
 }
+
 # We associate our route table to the public subnet in AZ 2
 # Makes the subnet public because it has a route to the internet via our internet gateway
 resource "aws_route_table_association" "associate_routetable_to_public_subnet_2" {
@@ -121,16 +129,19 @@ resource "aws_route_table_association" "associate_routetable_to_public_subnet_2"
    subnet_id = aws_subnet.public_subnet_2.id
    route_table_id = aws_route_table.IG_route_table.id
 }
+
 # We create an elastic IP for our NAT gateway in public subnet AZ 1
 # A static public IP address that we can assign to any EC2 instance
 resource "aws_eip" "elastic_ip_1" {
    vpc = true
 }
+
 # We create an elastic IP for our NAT gateway in public subnet AZ 2
 # A static public IP address that we can assign to any EC2 instance
 resource "aws_eip" "elastic_ip_2" {
    vpc = true
 }
+
 # We create a NAT gateway with a required public IP in public subnet AZ 1
 # Lives in a public subnet and prevents externally initiated traffic to our private subnet
 # Allows initiated outbound traffic to the Internet or other AWS services
@@ -145,6 +156,7 @@ resource "aws_nat_gateway" "nat_gateway_1" {
       Name = "nat-gateway-1"
    }
 }
+
 # We create a NAT gateway with a required public IP in public subnet AZ 2
 # Lives in a public subnet and prevents externally initiated traffic to our private subnet
 # Allows initiated outbound traffic to the Internet or other AWS services
@@ -159,6 +171,7 @@ resource "aws_nat_gateway" "nat_gateway_2" {
       Name = "nat-gateway-2"
    }
 }
+
 # We need 2 private routetables because each is associated to a specific NAT gateway id
 # We create a route table with target as NAT gateway 1 and destination as "internet"
 # Set of rules used to determine where network traffic is directed
@@ -176,6 +189,7 @@ resource "aws_route_table" "NAT_route_table_1" {
       Name = "NAT-route-table-1"
    }
 }
+
 # We create a route table with target as NAT gateway 2 and destination as "internet"
 # Set of rules used to determine where network traffic is directed
 resource "aws_route_table" "NAT_route_table_2" {
@@ -192,6 +206,7 @@ resource "aws_route_table" "NAT_route_table_2" {
       Name = "NAT-route-table-2"
    }
 }
+
 # We associate our NAT route table to the private subnet 1 in AZ 1
 # Keeps the subnet private because it has a route to the internet via our NAT gateway 
 resource "aws_route_table_association" "associate_routetable_to_private_subnet_1" {
@@ -202,6 +217,7 @@ resource "aws_route_table_association" "associate_routetable_to_private_subnet_1
    subnet_id = aws_subnet.private_subnet_1.id
    route_table_id = aws_route_table.NAT_route_table_1.id
 }
+
 # We associate our NAT route table to the private subnet 2 in AZ 2
 # Keeps the subnet private because it has a route to the internet via our NAT gateway
 resource "aws_route_table_association" "associate_routetable_to_private_subnet_2" {
@@ -212,6 +228,7 @@ resource "aws_route_table_association" "associate_routetable_to_private_subnet_2
    subnet_id = aws_subnet.private_subnet_2.id
    route_table_id = aws_route_table.NAT_route_table_2.id
 }
+
 # We create a security group for SSH traffic
 # EC2 instances' firewall that controls incoming and outgoing traffic
 resource "aws_security_group" "sg_bastion_host" {
@@ -245,40 +262,47 @@ resource "aws_security_group" "sg_bastion_host" {
       Name = "sg bastion host"
    }
 }
+
 # We create an elastic IP for our bastion host in public subnet 1 in AZ 1
 # A static public IP address that we can assign to our bastion host
 resource "aws_eip" "bastion_elastic_ip_1" {
    vpc = true
 }
+
 # We create an elastic IP for our bastion host in public subnet 2 in AZ 2
 # A static public IP address that we can assign to our bastion host
 resource "aws_eip" "bastion_elastic_ip_2" {
    vpc = true
 }
+
 # We create an ssh key using the RSA algorithm with 4096 rsa bits
 # The ssh key always includes the public and the private key
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
 # We upload the public key of our created ssh key to AWS
 resource "aws_key_pair" "public_ssh_key" {
   key_name   = var.public_key_name
   public_key = tls_private_key.ssh_key.public_key_openssh
    depends_on = [tls_private_key.ssh_key]
 }
+
 # We save our public key at our specified path.
 # Can upload on remote server for ssh encryption
 resource "local_file" "save_public_key" {
   content = tls_private_key.ssh_key.public_key_openssh 
   filename = "${var.key_path}${var.public_key_name}.pem"
 }
+
 # We save our private key at our specified path.
 # Allows private key instead of a password to securely access our instances
 resource "local_file" "save_private_key" {
   content = tls_private_key.ssh_key.private_key_pem
   filename = "${var.key_path}${var.private_key_name}.pem"
 }
+
 # We create a bastion host in public subnet 1 in AZ 1
 # Allows SSH into instances in private subnet
 resource "aws_instance" "bastion_host_1" {
@@ -304,6 +328,7 @@ resource "aws_instance" "bastion_host_1" {
     }
   }
 }
+
 # We create a bastion host in public subnet 2 in AZ 2
 # Allows SSH into instances in private subnet
 resource "aws_instance" "bastion_host_2" {
@@ -329,16 +354,19 @@ resource "aws_instance" "bastion_host_2" {
     }
   }
 }
+
 # We associate the elastic ip to our bastion host 1
 resource "aws_eip_association" "bastion_eip_association_1" {
   instance_id   = "${aws_instance.bastion_host_1.id}"
   allocation_id = "${aws_eip.bastion_elastic_ip_1.id}"
 }
+
 # We associate the elastic ip to our bastion host 2
 resource "aws_eip_association" "bastion_eip_association_2" {
   instance_id   = "${aws_instance.bastion_host_2.id}"
   allocation_id = "${aws_eip.bastion_elastic_ip_2.id}"
 }
+
 # We create a security group for our application load balancer
 # EC2 instances' firewall that controls incoming and outgoing traffic
 resource "aws_security_group" "sg_load_balancer" {
@@ -374,6 +402,7 @@ resource "aws_security_group" "sg_load_balancer" {
     aws_vpc.vpc
   ]
 }
+
 # We create a target group for our application load balancer
 resource "aws_alb_target_group" "tg_load_balancer" {
   name     = "target-group-load-balancer"
@@ -384,6 +413,7 @@ resource "aws_alb_target_group" "tg_load_balancer" {
     aws_vpc.vpc
   ]
 }
+
 # We create our application load balancer
 resource "aws_alb" "load_balancer" {
   name               = "load-balancer"
@@ -401,6 +431,7 @@ resource "aws_alb" "load_balancer" {
     aws_subnet.public_subnet_2
   ]
 }
+
 # We create a listener for our application load balancer
 resource "aws_alb_listener" "listener_load_balancer" {
   load_balancer_arn = aws_alb.load_balancer.id
@@ -415,6 +446,7 @@ resource "aws_alb_listener" "listener_load_balancer" {
     aws_alb_target_group.tg_load_balancer
   ]
 }
+
 # We create a security group for our wordpress instance
 resource "aws_security_group" "security_group_wordpress" {
   depends_on = [
@@ -453,6 +485,7 @@ resource "aws_security_group" "security_group_wordpress" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_launch_configuration" "wordpress_instance" {
   name_prefix   = "wordpress-instance-"
   image_id      = var.ec2_ami
@@ -466,6 +499,7 @@ resource "aws_launch_configuration" "wordpress_instance" {
     aws_security_group.security_group_wordpress
   ]
 }
+
 resource "aws_autoscaling_group" "auto_scaling_wordpress_az_1" {
   name                 = "auto-scaling-wordpress-az-1"
   launch_configuration = aws_launch_configuration.wordpress_instance.name
@@ -482,6 +516,7 @@ resource "aws_autoscaling_group" "auto_scaling_wordpress_az_1" {
     aws_alb_target_group.tg_load_balancer
   ]
 }
+
 resource "aws_autoscaling_group" "auto_scaling_wordpress_az_2" {
   name                 = "auto-scaling-wordpress-az-2"
   launch_configuration = aws_launch_configuration.wordpress_instance.name
