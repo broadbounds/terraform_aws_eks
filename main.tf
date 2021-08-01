997393c4b186995c2569671fde4d96a21e2d93da
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-31 20:40:13 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -838,7 +838,7 @@ locals {
  worker_nodes_userdata = <<USERDATA
  #!/bin/bash
  set -o xtrace
- /etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority.0.data}' '${var.cluster-name}'
+ /etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster.certificate_authority.0.data}' '${var.cluster_name}'
  USERDATA
 }
 
